1d2bb83434f2ad9452e8683436890d6a301f2d28
TsubasaYeung <tsubasayeung@gmail.com>
2019-06-06 14:54:54 +0800

    - 添加 快捷启动 - 完善 逻辑

--- check_code/check_code.py
+++ check_code/check_code.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 
+# Win32
 import os # 系统
 import re # 正则
 import time  # 时间
 import json # json
+import platform
+# OS X
+import subprocess
+
+def openFileSys(path):
+    if platform.system() == 'Windows':
+        os.startfile(path)
+    else:
+        subprocess.call(["open", path])
 
 class CheckCode:
     @staticmethod
@@ -15,8 +25,8 @@ class CheckCode:
 
         time_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         groupNameMap = {
-            "$$检查时间$$": [time_str],
-            "$$检查路径$$": [srcDir],
+            "检查时间": [time_str, "$$"],
+            # "检查路径": [srcDir, "$$"],
         }
         print (json.dumps(groupNameMap, ensure_ascii=False, indent=2))
 
@@ -26,7 +36,7 @@ class CheckCode:
                 fileshortname, ext = os.path.splitext(filename) # 后缀名
                 fullpath = os.path.join(root, filename) # 完整路径
                 relativepath = os.path.relpath(fullpath, srcDir) # 相对路径
-                regexp = '\\sid="(\\S+)"'
+                regexp = '\\sgroupName="(\\S+)"'
                 if ext == '.exml': # 后缀名为 .exml 的文件
                     print(fullpath)
                     with open(fullpath, 'r', encoding='utf-8') as file:
@@ -41,10 +51,20 @@ class CheckCode:
                                 matchArr = groupNameMap[match]
                             if not relativepath in matchArr: 
                                 matchArr.append(relativepath)
-
+        # 字典dict不能在遍历时修改
+        # for key in groupNameMap:
+        #     groupNames = groupNameMap[key]
+        #     if len(groupNames) == 1:
+        #         del groupNameMap[key]
+        for key in list(groupNameMap.keys()):
+            groupNames = groupNameMap[key]
+            if len(groupNames) == 1:
+                del groupNameMap[key]
         # 把结果写入指定文件
         with open(dstPath, 'w', encoding='utf-8') as file:
             json.dump(groupNameMap, file, indent=4, ensure_ascii=False)
+        openFileSys(dstDir)
+        openFileSys(dstPath)
 
 if __name__ == '__main__':
     CheckCode.check()
