2a2e4e759e12b9ec5771dea32dabdce24eeca385
TsubasaYeung <865500815@qq.com>
2019-06-27 04:41:50 +0800

    - init

---
+++ 2d_test/Assets/scripts/BaseMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovement : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // return;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(horizontal, vertical, 0.0f);
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
+        animator.SetFloat("Magnitude", movement.magnitude);
+        transform.position += movement * Time.deltaTime;
+    }
+}
