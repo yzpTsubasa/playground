2dbe09de9eb139b1041847ead78361993f8a598c
TsubasaYeung <865500815@qq.com>
2019-08-24 23:55:31 +0800

    工具栏、状态栏、菜单栏、快捷键

--- pyqt/MyApp.py
+++ pyqt/MyApp.py
@@ -26,7 +26,64 @@ class MainWindow(QMainWindow):
         widget.setLayout(layout)
 
         self.setCentralWidget(widget)
+
+        # 工具栏
+        toolbar = QToolBar("Tsubasa Toolbar")
+        toolbar.setIconSize(QSize(16, 16))
+        action = QAction(QIcon("./assets/img/acorn.png"), "Tsubasa Action", self)
+        action.setStatusTip("This is Tsubasa Action Button")
+        action.triggered.connect(self.onActionTriggered)
+        action.setCheckable(True)
+        toolbar.addAction(action)
+
+        # 快捷键
+        # action.setShortcut(QKeySequence(tr"ctrl+p"))
+        # action.setShortcut(QKeySequence(Qt.CTRL + Qt.Key_P))
+        action.setShortcut(QKeySequence(QKeySequence.Print))
+
+        toolbar.addSeparator()
+
+        action2 = QAction(QIcon("./assets/img/acorn.png"), "Tsubasa Action2", self)
+        action2.setStatusTip("This is Tsubasa Action2 Button")
+        action2.triggered.connect(self.onActionTriggered)
+        action2.setCheckable(True)
+        toolbar.addAction(action2)
+        # 添加分割线
+        toolbar.addSeparator()
+
+        toolbar.addWidget(QLabel("Hello"))
+        checkbox = QCheckBox()
+        checkbox.setStatusTip("Try me!!")
+        checkbox.toggled.connect(self.onCheckBoxToggled)
+        toolbar.addWidget(checkbox)
+
+        toolbar.addSeparator()
+
+        self.addToolBar(toolbar)
+
+        # 状态栏
+        self.setStatusBar(QStatusBar(self))
+
+        # 菜单栏
+        menu = self.menuBar()
+        menu.setNativeMenuBar(False) # 禁用 mac os 的全局菜单
+
+        file_menu = menu.addMenu(u"&文件")
+        file_menu.addAction(action)
+
+        file_menu.addSeparator() 
+        file_menu.addAction(action2)
+
+        # 子菜单
+        file_submenu = file_menu.addMenu(u"&取消")
+        file_submenu.addAction(action)
     
+    def onCheckBoxToggled(self, checked):
+        print("You are " + ("checked" if checked else "unchecked"))
+
+    def onActionTriggered(self, checked):
+        print("onActionTriggered", checked)
+
     def contextMenuEvent(self, e):
         print("Context menu requested!")
         super(MainWindow, self).contextMenuEvent(e)
