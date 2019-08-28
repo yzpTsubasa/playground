1768010d0c7547875404175e979883f1042de5b9
TsubasaYeung <tsubasayeung@gmail.com>
2019-08-28 16:51:39 +0800

    bro

--- pyqt/Browser.py
+++ pyqt/Browser.py
@@ -11,18 +11,25 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        # mac os 下要先调用这个方法
-        self.show()
-        self.setWindowTitle("My Browser")
-        self.setWindowIcon(QIcon("./assets/img/acorn.png"))
 
         # browser = QWebView()
         # browser.setUrl(QUrl("https://www.bilibili.com/"))
 
         # self.setCentralWidget(browser)
 
+        
+        # mac os 下要先调用这个方法
+        self.show()
+        self.setWindowTitle("My Browser")
+        self.setWindowIcon(QIcon("./assets/img/acorn.png"))
+
 
 app = QApplication(sys.argv)
+
+app.setApplicationName("Mozarella Ashbadger")
+app.setOrganizationName("Mozarella")
+app.setOrganizationDomain("mozarella.org")
+
 
 window = MainWindow()
 window.show()
