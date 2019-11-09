960f35a0ba199e5fae898fa0a89f773e5c40e225
TsubasaYeung <tsubasayeung@gmail.com>
2019-11-09 11:57:26 +0800

    添加 多层感知器

--- machine_learning/tensorflow_tutorial/lesson_03.py
+++ machine_learning/tensorflow_tutorial/lesson_03.py
@@ -1,3 +1,9 @@
 import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
 
-tf.keras
+data = pd.read_csv('./dataset/Income1.csv')
+print (data.head())
+plt.scatter(data.Education, data.Income)
+
+plt.show()
